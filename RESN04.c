8c672812223b8c131bfec4864f66e5eab63af42f
toycode <646035537@qq.com>
2012-08-20 20:06:57 +0800

    wrong problem

---
+++ RESN04.c
@@ -0,0 +1,14 @@
+/* spoj 5676. STONE GAME */ 
+/* wrong problem */
+#include <stdio.h>
+int main(){
+    int ct;
+    char s[2000];
+    scanf("%d", &ct);
+    while(ct --){
+    	gets(s);
+    	gets(s);
+    	puts("ALICE");
+    }
+    return 0;
+}
