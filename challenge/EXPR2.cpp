e8e539c5d5e730433a3ef78d16dd2bfad90f41c1
toycode <646035537@qq.com>
2012-08-22 10:24:46 +0800

    expr2

---
+++ challenge/EXPR2.cpp
@@ -0,0 +1,24 @@
+// 2004. Analyse Simple Arithmetical Expressions
+#include <cstdio>
+int main(){
+    if(int ct = 1){
+        if(scanf("%d", &ct)){}
+            while(ct--){
+                if(int a = 1){
+                    if(char o = '+'){
+                        if(int b = 1){
+                            if(scanf("%d%c%d", &a, &o, &b)){
+                            if(o == '+'){
+                                if(printf("%d\n", a + b)){}}
+                            else if(o == '-'){
+                                if(printf("%d\n", a - b)){}}
+                            else if(o == '*'){
+                                if(printf("%d\n", a * b)){}}
+                            }else{
+                                if(printf("%d\n", a / b)){}}
+                        }
+                    }
+                }
+            }
+    }
+}
